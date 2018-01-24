6a11b4464e04ec9931b7d5b82b3b9ae47a68a2bc
Robert Lord <robert@codepeak.se>
2018-01-24 20:34:49 +0100

    Inital commit with basic structure

---
+++ Cron/Sessions.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * A Magento 2 module named Codepeak_Optimize
+ * Copyright (C) 2018 Codepeak AB 2018
+ *
+ * This file is part of Codepeak_Optimize.
+ *
+ * Codepeak_Optimize is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Codepeak\Optimize\Cron;
+
+/**
+ * Class Sessions
+ *
+ * @package  Codepeak\Optimize\Cron
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @author   Robert Lord, Codepeak AB <robert@codepeak.se>
+ * @link     https://codepeak.se
+ */
+class Sessions
+{
+    /**
+     * Execute the cron
+     *
+     * @return void
+     */
+    public function execute()
+    {
+        // Do the magic clean up here
+    }
+}
